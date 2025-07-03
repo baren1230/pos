54fff253b74b6028234f533bbb82ba097e10fa55
baren1230 <125537282+baren1230@users.noreply.github.com>
2025-07-03 03:36:18 -0700

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,6 @@
-export default {
-    plugins: {
-        tailwindcss: {},
-        autoprefixer: {},
-    },
-};
+emodule.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
